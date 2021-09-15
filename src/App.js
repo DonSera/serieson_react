a1016243f79107dc8acc0a8a8255daa7e4bf994c
DonSera <sera.2021@lifeoasis.com>
2021-09-15 18:59:30 +0900

    debug : inline-block으로 인한 버그 수정

--- src/App.js
+++ src/App.js
@@ -1,69 +1,75 @@
 import './App.css';
 import MovieCard from "./component/MovieCard";
+import MovieCardSide from "./component/MovieCard/MovieCardSide";
 
 function App() {
     return (
         <div className="App">
-            <section className={`header`}>
-                <div className={`app-logo`}/>
-                <div className={`app-search`}/>
-            </section>
-            <section className={`movie-suggestion`}>
-                <div className={`suggestion`}>
-                    <MovieCard/>
-                    <MovieCard/>
-                    <MovieCard/>
-                    <MovieCard/>
-                    <MovieCard/>
-                </div>
-                <div>추천하기</div>
-            </section>
-            <section className={`movie-info`}>
-                <section className={`main-info`}>
-                    <div className={`adveres`}>
-                        <span className={`advertisement`}/>
-                        <span className={`advertisement`}/>
-                    </div>
-                    <div className={`recent`}>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                    </div>
-                    <div className={`discount`}>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                        <MovieCard/>
-                    </div>
+            <div className={`main`}>
+                <section className={`header`}>
+                    <div className={`app-logo`}/>
+                    <div className={`app-search`}/>
                 </section>
-                <section className={`side-info`}>
-                    <div className={`top10`}>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
-                        <div className={`movie-card`}/>
+                <section className={`movie-suggestion`}>
+                    <div className={`suggestion`}>
+                        <MovieCard/>
+                        <MovieCard/>
+                        <MovieCard/>
+                        <MovieCard/>
+                        <MovieCard/>
                     </div>
-                    <div className={`adver2`}/>
+                    <div>추천하기</div>
                 </section>
-            </section>
-            <section className={`app-service`}>
-                <div className={`event`}/>
-                <div className={`service`}/>
-            </section>
-            <section className={`footer`}/>
+                <section className={`movie-info`}>
+                    <section className={`main-info`}>
+                        <div className={`adveres`}>
+                            <span className={`advertisement`}/>
+                            <span className={`advertisement`}/>
+                        </div>
+                        <div className={`recent`}>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                        </div>
+                        <div className={`discount`}>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                            <MovieCard/>
+                        </div>
+                    </section>
+                    <section className={`side-info`}>
+                        <div className={`adver2`}/>
+                        <div className={`top10`}>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                            <MovieCardSide/>
+                        </div>
+                        <div className={`adver2`}/>
+                    </section>
+                </section>
+                <section className={`app-service`}>
+                    <div className={`event`}/>
+                    <div className={`service`}/>
+                </section>
+                <section className={`footer`}/>
+            </div>
         </div>
     );
 }
