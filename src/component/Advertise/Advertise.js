116b1dc92f7238a9bafdd70a8e456253d689fb60
DonSera <sera.2021@lifeoasis.com>
2021-09-16 19:01:49 +0900

    feat : 광고 방향 정해주기

--- src/component/Advertise/Advertise.js
+++ src/component/Advertise/Advertise.js
@@ -2,7 +2,7 @@ import styles from './Advertise.module.css'
 
 function Advertise(props){
 return(
-    <div className={styles[`adver`]} style={{width: props.width, height: props.height}}>광고</div>
+    <div className={styles[`adver`]} style={{width: props.width, height: props.height, float: props.dirction}}>광고</div>
 )
 }
 
