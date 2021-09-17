4822e335282e3fe97396b2df123f5d5e459520d7
DonSera <sera.2021@lifeoasis.com>
2021-09-17 12:22:29 +0900

    style : 광고 위치 잡기

--- src/component/Advertise/Advertise.js
+++ src/component/Advertise/Advertise.js
@@ -1,9 +1,13 @@
 import styles from './Advertise.module.css'
 
-function Advertise(props){
-return(
-    <div className={styles[`adver`]} style={{width: props.width, height: props.height, float: props.dirction}}>광고</div>
-)
+function Advertise({width, height, float='top'}) {
+    return (
+        <div className={styles[`adver`]} style={{
+            width: width,
+            height: height,
+            float: float
+        }}>광고</div>
+    )
 }
 
 export default Advertise;
