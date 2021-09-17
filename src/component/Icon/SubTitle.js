5328c6fd2a3bb06db7191009e5d339deb20f9c59
DonSera <sera.2021@lifeoasis.com>
2021-09-17 15:32:32 +0900

    docs : SubYouth 파일 분리

---
+++ src/component/Icon/SubTitle.js
@@ -0,0 +1,7 @@
+import styles from './SubTitle.module.css'
+
+function SubTitle() {
+    return <span className={styles[`sub`]}>자막</span>;
+}
+
+export default SubTitle;
---
+++ src/component/Icon/Youth.js
@@ -0,0 +1,7 @@
+import styles from './Youth.module.css'
+
+function Youth(){
+    return <span className={styles[`youth`]}>청불</span>;
+}
+
+export default Youth;
