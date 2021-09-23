03518d56c7593b0e4c825e2a80b5d9230ff9678b
DonSera <sera.2021@lifeoasis.com>
2021-09-23 11:50:12 +0900

    feat : 임의 광고 추가

--- src/component/Advertise/Advertise.js
+++ src/component/Advertise/Advertise.js
@@ -1,13 +1,23 @@
 import styles from './Advertise.module.css'
 
-function Advertise({width, height, float='top'}) {
-    return (
-        <div className={styles[`adver`]} style={{
+function Advertise({width, height, urlImg, urlLink, float = 'top'}) {
+    if (urlImg) {
+        return <a className={styles[`adver`]} href={urlLink} style={{
             width: width,
             height: height,
             float: float
-        }}>광고</div>
-    )
+        }}>
+            <img className={styles['url-img']} src={urlImg} alt={'광고'}/>
+        </a>;
+    } else {
+        return <div className={styles[`adver`]} style={{
+            width: width,
+            height: height,
+            float: float
+        }}>광고</div>;
+    }
+
+
 }
 
 export default Advertise;
