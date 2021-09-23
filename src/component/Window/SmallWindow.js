2f004b5cb2fc7681250da87f39384e0941e0227b
DonSera <sera.2021@lifeoasis.com>
2021-09-23 11:14:19 +0900

    feat : hight 없이 window 만들기 가능

--- src/component/Window/SmallWindow.js
+++ src/component/Window/SmallWindow.js
@@ -1,12 +1,17 @@
 import styles from './SmallWindow.module.css'
 
-function SmallWindow({width, height, header, contents}) {
-    return (
-        <div className={styles['window-background']} style={{width: width, height:height}}>
-            <div className={styles['window-header']} style={{height: header}}/>
+function SmallWindow({width, height = 0, header, contents}) {
+    if (height) {
+        return <div className={styles['window-background']} style={{width: width, height: height}}>
+            <div className={styles['window-header']} style={{height: header}}> header </div>
             <div className={styles['window-main']}>{contents}</div>
-        </div>
-    );
+        </div>;
+    } else {
+        return <div className={styles['window-background']} style={{width: width}}>
+            <div className={styles['window-header']} style={{width: width, height: header}}>Header</div>
+            <div className={styles['window-main']} style={{width: width}}>{contents}</div>
+        </div>;
+    }
 }
 
 export default SmallWindow;
