68c2c46dc9bf56db95ab57ddc249ada747c84d00
DonSera <sera.2021@lifeoasis.com>
2021-09-24 12:00:34 +0900

    feat : 글씨 클릭시 배경 색상 변경

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,41 +1,58 @@
 import styles from './Header.module.css'
+import React from "react";
 
-function Header({header, headerDetail}) {
+class Header extends React.Component {
+    state = {
+        colorArray : Array(this.props.header.length).fill(false)
+    }
 
-    return (
-        <div id={styles['headerWarp']}>
-            <div id={styles['logo']}>
-                <a id={'naver'} href={"https://www.naver.com/"}>Naver</a>
-                <span id={'series'}>SERIES_on</span>
-                <span id={'login'}>로그인</span>
-                <span id={'menu'}>메뉴</span>
-            </div>
+    onChange(index) {
+        console.log("change color");
+        const newColorArray = Array(this.props.header.length).fill(false);
+        newColorArray[index] = true;
+        this.setState({
+            colorArray : newColorArray
+        })
+    }
 
-            <div className={styles[`divider`]}/>
-            <div id={styles['category']}>
-                <ul>
-                    {
-                        header.map((text,index) => <li key={`header_text_${index}`}>{text}</li>)
-                    }
-                    <span>
+    render() {
+        return (
+            <div id={styles['headerWarp']}>
+                <div id={styles['logo']}>
+                    <a id={'naver'} href={"https://www.naver.com/"}>Naver</a>
+                    <span id={'series'}>SERIES_on</span>
+                    <span id={'login'}>로그인</span>
+                    <span id={'menu'}>메뉴</span>
+                </div>
+
+                <div className={styles[`divider`]}/>
+                <div id={styles['category']}>
+                    <ul>
+                        {
+                            this.props.header.map((text, index) => <li key={`header_text_${index}`}
+                                                                       style={{backgroundColor: this.state.colorArray[index] ? "green" : ""}}
+                                                                       onClick={() => this.onChange(index)}>{text}</li>)
+                        }
+                        <span>
                         <span>My</span>
                         <span>장바구니</span>
                         <span>검색</span>
                     </span>
-                </ul>
-            </div>
+                    </ul>
+                </div>
 
-            <div className={styles[`divider`]}/>
-            <div id={styles['detail']}>
-                <ul>
-                    {
-                        headerDetail.map((text,index) => <li key={`detail_text_${index}`}>{text}</li>)
-                    }
-                </ul>
+                <div className={styles[`divider`]}/>
+                <div id={styles['detail']}>
+                    <ul>
+                        {
+                            this.props.headerDetail.map((text, index) => <li key={`detail_text_${index}`}>{text}</li>)
+                        }
+                    </ul>
+                </div>
+                <div className={styles[`divider`]}/>
             </div>
-            <div className={styles[`divider`]}/>
-        </div>
-    );
+        );
+    }
 }
 
 export default Header;
