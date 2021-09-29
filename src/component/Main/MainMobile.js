e0e9a9e3ebfd20fcb5c4ac5bd395bef22b077474
DonSera <sera.2021@lifeoasis.com>
2021-09-29 18:40:57 +0900

    feat : Mobile 광고(drag) 추가

--- src/component/Main/MainMobile.js
+++ src/component/Main/MainMobile.js
@@ -1,7 +1,13 @@
 import './MainMobile.css';
 import sortRank from "../../function/sortRank";
 import MovieCardMobile from "../MovieCard/MovieCardMobile";
+import Advertise from "../Advertise/Advertise";
 import {useEffect, useState} from "react";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {useMediaQuery} from "react-responsive";
 
 function MainMobile({movieInfo}) {
     const [sort, setSort] = useState([])
@@ -11,21 +17,115 @@ function MainMobile({movieInfo}) {
     }, [movieInfo])
 
 
-    function advertise() {
+    const two = useMediaQuery({
+        query: "(min-width:400px)"
+    });
+
+    const one = useMediaQuery({
+        query: "(max-width:400px)"
+    });
+
+    const settingsOne = {
+        arrows: false,
+        draggable: true,
+        dots: false,
+        speed: 500,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+    const settingsTwo = {
+        arrows: false,
+        draggable: true,
+        dots: false,
+        speed: 500,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    };
+
+
+    function advertiseTop() {
         return (
-            <div className={'adver-background'}>
-                <div className={'adver'}/>
-            </div>
+            <Slider {...settingsOne}>
+                <Advertise width={'100%'}
+                           height={'40%'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'100%'}
+                           height={'40%'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'100%'}
+                           height={'40%'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+            </Slider>
+        )
+    }
+
+    function advertiseTwo() {
+        return (
+            <Slider {...settingsTwo}>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+            </Slider>
+        )
+    }
+
+    function advertiseOne() {
+        return (
+            <Slider {...settingsOne}>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+                <Advertise width={'400px'}
+                           height={'150px'}
+                           urlImg={"https://nstore-phinf.pstatic.net/20210914_293/nstore_adm_1631619930201MrJSw_JPEG/4onPC_344X129.jpg"}
+                           urlLink={"https://serieson.naver.com/event/details.nhn?eventNo=13564"}/>
+            </Slider>
         )
     }
 
     return <div>
-        {advertise()}
+        {advertiseTop()}
         {sort.map((obj, index) => {
             if (index === 4) {
                 return (
                     <>
-                        {advertise()}
+                        {one && advertiseOne()}
+                        {two && advertiseTwo()}
                         <MovieCardMobile key={`side_${obj.name}_${index}`} obj={obj}/>
                     </>
                 )
